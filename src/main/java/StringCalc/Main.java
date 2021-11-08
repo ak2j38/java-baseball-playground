e8778b2f80e5d18c7531a83ba178d622e2732aa0
ak2j38 <k2j38@naver.com>
2021-11-08 20:11:26 +0900

    문자열 계산기 및 테스트 코드 추가

--- src/main/java/StringCalc/Main.java
+++ src/main/java/StringCalc/Main.java
@@ -1,2 +1,44 @@
-package StringCalc;public class Main {
+package StringCalc;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // input
+        Scanner sc = new Scanner(System.in);
+        Queue<String> operators = new LinkedList<>();
+        Queue<Integer> operands = new LinkedList<>();
+
+        System.out.println("사칙연산 문자열을 입력하세요 : ");
+        String input = sc.nextLine();
+        String[] split = input.split(" ");
+
+        for(String str : split){
+            if(str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/")){
+                operators.add(str);
+                continue;
+            }
+            operands.add(Integer.parseInt(str));
+        }
+
+        // calc
+        int sum = operands.poll();
+        for(String operator : operators){
+            int operand = operands.poll();
+            sum = calc(sum, operand, operator);
+        }
+
+        // output
+        System.out.println("결과는 " + sum + " 입니다.");
+    }
+
+    public static int calc(int operand1, int operand2, String operator){
+        if(operator.equals("+")) return operand1 + operand2;
+        if(operator.equals("-")) return operand1 - operand2;
+        if(operator.equals("*")) return operand1 * operand2;
+        return operand1 / operand2;
+    }
 }
--- src/test/java/StringCalc/StringCalcTest.java
+++ src/test/java/StringCalc/StringCalcTest.java
@@ -1,2 +1,37 @@
-package StringCalc;public class StringCalcTest {
+package StringCalc;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringCalcTest {
+
+    private Main main;
+
+    @BeforeEach
+    void setMain(){
+        main = new Main();
+    }
+
+    @Test
+    void 덧셈테스트(){
+        assertThat(main.calc(1, 2, "+")).isEqualTo(3);
+    }
+
+    @Test
+    void 뺄셈테스트(){
+        assertThat(main.calc(4, 2, "-")).isEqualTo(2);
+    }
+
+    @Test
+    void 곱셈테스트(){
+        assertThat(main.calc(5, 2, "*")).isEqualTo(10);
+    }
+
+    @Test
+    void 나눗셈테스트(){
+        assertThat(main.calc(10, 2, "/")).isEqualTo(5);
+    }
+
 }

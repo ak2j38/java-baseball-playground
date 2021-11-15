8d81c0a91d3d0a3309e4c12b4eb0fcaecd2f4ca8
ak2j38 <k2j38@naver.com>
2021-11-16 00:28:57 +0900

    feature(baseball) : create skeleton code

---
+++ src/main/java/BaseballFeedback/Judgement.java
@@ -0,0 +1,14 @@
+package BaseballFeedback;
+
+import java.util.List;
+
+public class Judgement {
+
+    public int correctCount(List<Integer> computer, List<Integer> player) {
+        return 0;
+    }
+
+    public boolean hasPlace(int place, int number) {
+        return false;
+    }
+}

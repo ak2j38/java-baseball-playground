5c29e43f7413dc46c3ddfc23a367c83497d73ab0
ak2j38 <k2j38@naver.com>
2021-11-20 00:55:22 +0900

    refactor : Main#main 로직 제거하고 BaseballRule 객체를 통해 게임시작으로 변경

--- src/main/java/BaseballFeedback/Main.java
+++ src/main/java/BaseballFeedback/Main.java
@@ -1,36 +1,9 @@
 package BaseballFeedback;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        NumberGenerator generator = new NumberGenerator();
-        List<Integer> computerNumbers = generator.createRandomNumbers();
-
-        Referee re = new Referee();
-
-        String result = "";
-        while (!result.equals("3스트라이크")) {
-            result = re.compare(computerNumbers, askNumbers());
-            System.out.println(result);
-        }
-        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-    }
-
-    public static List<Integer> askNumbers()  {
-        System.out.print("숫자를 입력해주세요 : ");
-        Scanner sc = new Scanner(System.in);
-        String playerInput = sc.nextLine();
-
-        List<Integer> playerNumbers = new ArrayList<>();
-
-        for (String number : playerInput.split("")) {
-            playerNumbers.add(Integer.parseInt(number));
-        }
-        return playerNumbers;
+        BaseballRule baseballRule = new BaseballRule();
+        baseballRule.startGame();
     }
 }

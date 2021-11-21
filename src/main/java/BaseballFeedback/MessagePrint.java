7affb18035d2d74ca582b30fc4781062e3e7e4d1
ak2j38 <k2j38@naver.com>
2021-11-22 00:19:36 +0900

    feature(baseball) : MessagePrint#printInputMessage() - 플레이어 숫자 입력요청 메시지 출력

--- src/main/java/BaseballFeedback/MessagePrint.java
+++ src/main/java/BaseballFeedback/MessagePrint.java
@@ -5,4 +5,9 @@ public class MessagePrint {
     public static void printEndMessage() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
     }
+
+    public static void printInputMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
 }
